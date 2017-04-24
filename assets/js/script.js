d81d69947d86363bd71495df3a1918d6ff9d2d1b
fschaeferbastian <fridaschaeferbastia@berkeley.edu>
2017-04-23 21:26:40 -0700

    commit 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 
-	$(".portfolio-button").click(function(){
+	$("#portfolio-button").click(function(e){
+		e.preventDefault();
+		$('html, body').animate({scrollTop: $("#slide2").offset().top}, 300);
 	});
 
+	$("#about-button").click(function(e){
+		e.preventDefault();
+		$('html, body').animate({scrollTop: $("#slide3").offset().top -59}, 300);
+
+	});
+
+$("#bigname").fadeIn(1000);
+
+$("ul.image li").hover
 });
